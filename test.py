2fc9e42f670587153d41e08940bb2166359c8464
ma-saffa <ma_saffa@hotmail.com>
2021-01-20 17:08:12 -0500

    tttttttttttt

--- test.py
+++ test.py
@@ -1,8 +1,2 @@
 msg = "HELLLOOOO"
 print(msg) 
-from cli import *
-import json
-cmd1 = 'show ip int brief | json-pretty'
-json_data = cli(cmd1)
-json_final = json.loads(json_data)
-print(json_final['TABLE_intf']['ROW_intf'][0]['prefix'])
